26e6081c8f38ba4c47f24c50ee826c52ac094b0d
Yichu Liu <nellie@nellies-MacBook-Air.local>
2020-03-31 15:06:05 -0700

    different components design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ServiceIpscGenComponent } from './service-ipsc-gen/service-ipsc-gen.component';
+import { FooterComponent } from './footer/footer.component';
+import { ServicesComponent } from './services/services.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServiceIpscGenComponent
+    ServiceIpscGenComponent,
+    FooterComponent,
+    ServicesComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
